43d72449bffc9cf95630e91e23731b744402a330
Mohamed Laradji <malaradji@gmail.com>
2018-09-05 00:12:19 +0000

    Edit copy.f

--- copy.f
+++ copy.f
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+# Change the working directory.
+
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+cd "$parent_path"
+
+
+declare -a files=(main.py nohup.out checkpoint.pth.tar model_best.pth.tar launch.sh resume.sh test.sh)
+
+
 
 function from
 {
-	declare -a files=(main.py nohup.out checkpoint.pth.tar model_best.pth.tar launch.sh)
         for i in "${files[@]}"
                 do
                 cp trials/"$1"/"$i" .  
@@ -13,7 +22,6 @@ function from
 
 function to
 {
-	declare -a files=(main.py nohup.out checkpoint.pth.tar model_best.pth.tar launch.sh)
 	for i in "${files[@]}"
 		do
 		mkdir -p trials/"$1"
